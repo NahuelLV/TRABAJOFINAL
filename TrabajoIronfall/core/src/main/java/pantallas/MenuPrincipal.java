3150efb0fb34beec83f3f396c9894057c0e0223c
SantinoGiuffrida <130598051+Santino-Giuffrida-Castillo@users.noreply.github.com>
2025-07-23 15:53:15 -0300

    Creo las pantallas principales y pruebo como funciona LIBgdx

---
+++ TrabajoIronfall/core/src/main/java/pantallas/MenuPrincipal.java
@@ -0,0 +1,90 @@
+package pantallas;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import pro.juego.Ironfall.Ironfalljuego;
+
+
+
+public class MenuPrincipal implements Screen {
+
+    private Ironfalljuego juego;
+    private Stage stage;
+    private Skin skin;
+
+    public MenuPrincipal(Ironfalljuego juego) {
+        this.juego = juego;
+        this.stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void show() {
+        skin = new Skin(Gdx.files.internal("uiskin.json")); // skin predeterminado
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        TextButton btnUnJugador = new TextButton("Un Jugador", skin);
+        TextButton btnMultijugador = new TextButton("Multijugador", skin);
+        TextButton btnSalir = new TextButton("Salir", skin);
+
+        // Listener para "Un Jugador"
+        btnUnJugador.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                juego.setScreen(new PantallaJuego(juego)); // Ir a juego
+            }
+        });
+
+        // Listener para "Multijugador" (todavía sin implementación)
+        btnMultijugador.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Multijugador (pendiente)");
+            }
+        });
+
+        // Listener para "Salir"
+        btnSalir.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        table.add(btnUnJugador).pad(20).row();
+        table.add(btnMultijugador).pad(20).row();
+        table.add(btnSalir).pad(20);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+    @Override public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
+}
